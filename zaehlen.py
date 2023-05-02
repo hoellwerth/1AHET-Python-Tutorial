431258ba41be899b46ad2be64f93be64d957036e
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-05-02 20:36:48 +0200

    Added Documentation for zaehlen.py

--- zaehlen.py
+++ zaehlen.py
@@ -1,16 +1,16 @@
+def zaehlen_while(untergrenze, obergrenze): # Definiert Funktion
+  count = untergrenze                       # Setzt count auf untergrenze
+                                            # 
+  while count <= obergrenze:                # Solange count kleiner gleich obergrenze
+    print(count)                            # Ausgabe von count
+    count += 1                              # count wird um 1 erhöht
+                                            #
+def zaehlen_for(untergrenze, obergrenze):   # Definiert Funktion
+  for i in range(untergrenze, obergrenze):  # Wiederhole für i in range(untergrenze, obergrenze)
+    print(i)                                # Ausgabe von i
+
 untergrenze = int(input("Gib untergrenze ein: "))
 obergrenze = int(input("Gib obergrenze ein: "))
-
-def zaehlen_while(untergrenze, obergrenze):
-  count = untergrenze
-
-  while count <= obergrenze:
-    print(count)
-    count += 1
-
-def zaehlen_for(untergrenze, obergrenze):
-  for count in range(untergrenze, obergrenze):
-    print(count)
 
 zaehlen_while(untergrenze, obergrenze)
 zaehlen_for(untergrenze, obergrenze)
