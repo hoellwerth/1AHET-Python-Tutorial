ffbeac043d539e866329b45625f3b581f4a7c2a6
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-05-02 20:50:57 +0200

    Updated Imports for dreiecksspirale.py

--- dreiecksspirale.py
+++ dreiecksspirale.py
@@ -1,16 +1,16 @@
-import turtle
+from turtle import *
 
-turtle.shape('turtle')
-turtle.speed(10)
+shape('turtle')
+speed(10)
 
-turtle.color('white')
-turtle.bgcolor('black')
+color('white')
+bgcolor('black')
 
 for i in range(300, 0, -10):
     for n in range(3):
-        turtle.forward(i)
-        turtle.left(120)
+        forward(i)
+        left(120)
 
-    turtle.left(10)
+    left(10)
 
-turtle.exitonclick()
+exitonclick()
