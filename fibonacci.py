17c577371c2ca184a040a24f637ee025ec194dd7
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-05-10 16:33:36 +0200

    Updated fibonnaci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,8 @@
-def fibonacci(start, end):      # Definiert fibonacci Funktion mit Parameter start und end
-  temp = 0                      # Setzt temporäre variable auf 0
-  for i in range(start, end+1): # Wiederhole von start bis end+1 (end ist inkludiert)
-    temp += i                   # Addiert i zu temp
-    print(f"{i}: {temp}")       # Gibt i und temp in formatierten string aus
-                                # 
-fibonacci(1, 10)                # Führt fibonacci Funktion mit start=1 und end=10 aus
+def fibonnaci(end, start = 0):
+    a, b = 0, 1
+
+    for _ in range(start, end):
+        print(a, end=" \n")
+        a,b = b, a + b
+
+fibonnaci(10)
