6a21b90b2c13896351489216743ca739d456e5ff
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-10-08 11:53:31 +0200

    Updated wertetabelle format

--- wertetabelle(while).py
+++ wertetabelle(while).py
@@ -14,5 +14,7 @@ while start <= end:
     if abs(start - 0) < 0.0000000001:
         print("|           0        |        0           |  NICHT DEFINIERT   |")
     else:
-        print("|      {0:8.4}      |      {1:8.4f}      |      {2:8.4f}      |".format(start, math.pow(start, 2), 1 / start))
+        print("|      {0:8.4}      |      {1:8.4f}      |      {2:8.4f}      |"
+              .format(start, math.pow(start, 2), 1 / start)
+              )
     start += step
